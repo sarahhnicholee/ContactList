f53eb697143773789ea12e0a0b5b25350be7833c
Sarah Parent <sarahparent@Sarahs-MacBook-Air.local>
2024-10-31 08:35:17 -0500

    almost finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
-
+// import { useState } from 'react'
+import ContactList from './components/ContactList.jsx'
 import './App.css'
+import {useState} from 'react'
+import SelectedContact from './components/SelectedContact.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+const [selectedContactId, setSelectedContactId]= useState(null);
   return (
     <>
-     
+    {selectedContactId ? (
+      <div>Selected Contact View</div>
+    ):(
+     <ContactList/>
+    )}
     </>
-  )
+  );
 }
 
 export default App
---
+++ src/components/ContactRow.jsx
@@ -0,0 +1,19 @@
+import ContactList from "./ContactList"
+
+
+function ContactRow( {setSelectedContactId, contact} ) {
+ 
+    return (
+    
+    <tr onClick= {()=>{
+        setSelectedContactId(contact.id);
+    }}
+        >
+         <td> {contact.name} </td>
+         <td> {contact.email}</td>
+         <td> {contact.phone}</td>
+    </tr>
+    )
+  }
+  
+  export default ContactRow
